c12b9eefbdff8316906119e4bfec59a49cc891c0
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2025-09-05 20:12:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// log in button functionality
+document.getElementById("logInButton").addEventListener('click', function (e){
+    e.preventDefault();
+    console.log('log in button clicked');
+    console.log(e);
+})
