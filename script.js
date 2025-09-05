8d00276c10511bb3f4cd9e77d43a3dc6aad09838
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2025-09-06 00:30:23 +0600

    Add money page

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // log in button functionality
 document.getElementById("logInButton").addEventListener('click', function (e){
     e.preventDefault();
-    console.log('log in button clicked');
-    console.log(e);
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById('mobile-number').value
+    const pinNumberValue = document.getElementById('pin-number').value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    // console.log(mobileNumberValueConverted)
+    // console.log(pinNumberValueConverted)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert('Invalid Credentials\nThere is no Database Connections so use 12345678910 as Mobile Number and use 1234 as Pin Number')
+    }
 })
